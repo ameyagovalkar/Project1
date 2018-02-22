2e497ed60fcdb53b53f62740af50be7fddf53f41
Samantha Ibasitas <36210345+samanthaibasitas@users.noreply.github.com>
2018-02-21 21:06:24 -0800

    Update Registrar.cpp

--- Registrar.cpp
+++ Registrar.cpp
@@ -3,14 +3,11 @@
 #include <fstream>
 #include <stdexcept>
 #include "Registrar.h"
-
 using namespace std;
-
 // Load information from a text file with the given filename
 // THIS FUNCTION IS COMPLETE
 void Registrar::readTextfile(string filename) {
 	ifstream myfile(filename);
-
 	if (myfile.is_open()) {
 		cout << "Successfully opened file " << filename << endl;
 		string courseName;
@@ -25,16 +22,14 @@ void Registrar::readTextfile(string filename) {
 	else
 		throw invalid_argument("Could not open file " + filename);
 }
-
 // return Student object corresponding to a given CWID
 // getStudent must throw an exception if cwid is invalid
 Student& Registrar::getStudent(string cwid) const {
 	// TO BE COMPLETED
+	
 }
-
 // process a line from the text file
 void Registrar::addLine(string courseName, string cwid, char grade) {
 	// TO BE COMPLETED
 
 }
-
