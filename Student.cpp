e0364de0239a1edc68208bb0801c2fb32b7ea5c7
ameyagovalkar <36211515+ameyagovalkar@users.noreply.github.com>
2018-02-25 00:40:49 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,57 +1,53 @@
-=#include "Student.h"
+#include "Student.h"
 #include <iostream>
+
+using namespace std;
+
+
 Student::Student() {
 	// TO BE COMPLETED
-	cwid = " "; 
+	StudentCWID = " ";
 	courses = new string[50];
-	grades = new char[50]; 
-	gpa = 0.0; 
+	grades = new char[50];
+	gpa = 0.0;
 	counter = 0;
 }
-Student::Student(const string &cwid) {
-	// TO BE COMPLETED
-	cwid = cwid; 
-	courses = new string[50];
-	grades = new char[50]; 
-	gpa = 0.0; 
-	counter = 0; 
-}
 Student::~Student() {
-	delete [] courses; 
-	delete [] grades;
+	delete[] courses;
+	delete[] grades;
 }
 string Student::getCWID() {
 	// TO BE COMPLETED
-	return cwid; 
+	return StudentCWID;
 }
-void Student::addCourseGrade (const string &courseName, char grade) {
+void Student::addCourseGrade(const string &courseName, char grade) {
 	// TO BE COMPLETED
-	courses[counter] = courseName; 
-	grades[counter] = grade; 
-	if(counter >= 50)
-		counter = 50; 
+	courses[counter] = courseName;
+	grades[counter] = grade;
+	if (counter >= 50)
+		counter = 50;
 	else
-		counter++; 
+		counter++;
 }
 double Student::getGPA() {
 	// TO BE COMPLETED
-	double points = 0.0; 
-	for(int i = 0; i < counter; i++) {
-		if(grades[i] == 'A')
-			points += 4.0; 
-		else if(grades[i] == 'B')
-			points += 3.0; 
-		else if(grades[i] == 'C')
-			points += 2.0; 
-		else if(grades[i] == 'D')
-			points += 1.0; 
-		else if(grades[i] == 'F')
-			points += 0.0; 
-		else 
-			points += 0.0; 
+	double points = 0.0;
+	for (int i = 0; i < counter; i++) {
+		if (grades[i] == 'A')
+			points += 4.0;
+		else if (grades[i] == 'B')
+			points += 3.0;
+		else if (grades[i] == 'C')
+			points += 2.0;
+		else if (grades[i] == 'D')
+			points += 1.0;
+		else if (grades[i] == 'F')
+			points += 0.0;
+		else
+			points += 0.0;
 	}
-	gpa = points/counter; 
-	return gpa; 
+	gpa = points / counter;
+	return gpa;
 }
 // print transcript in this (sample) format:
 // TRANSCRIPT FOR CWID=279750343
@@ -59,11 +55,13 @@ double Student::getGPA() {
 // CS 253		B
 // CS 131		B
 // GPA = 2.6667
+
 void Student::printTranscript() {
 	// TO BE COMPLETED
-	cout << "TRANSCRIPT FOR CWID = " << cwid << endl; 
-	for(int i = 0; i < counter; i++) {
-		cout << courses[i] << "		" << grades[i] << endl; 
+
+	cout << "TRANSCRIPT FOR CWID = " << StudentCWID << endl;
+	for (int i = 0; i < counter; i++) {
+		cout << courses[i] << "		" << grades[i] << endl;
 	}
-	cout << "GPA = " << gpa; 
+	cout << "GPA = " << gpa;
 }
