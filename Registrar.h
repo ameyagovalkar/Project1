d4ed1d1dce38e514533f6b73d1afeaa056c9ee2a
ameyagovalkar <36211515+ameyagovalkar@users.noreply.github.com>
2018-02-25 00:41:14 -0800

    Update Registrar.h

--- Registrar.h
+++ Registrar.h
@@ -2,23 +2,26 @@
 #include <string>
 #include <stdexcept>
 #include "Student.h"
+
 using namespace std;
+
 class Registrar {
 public:
 	void readTextfile(string filename); // Load information from a text file with the given filename: THIS IS COMPLETE
 	void addLine(string courseName, string cwid, char grade); // process a line from the text file
 	Student& getStudent(string cwid) const; // return the Student object corresponding to a given CWID
-	// getStudent must throw an exception if cwid is invalid
-	// add constructors, destructors, assignment operators if needed
+											// getStudent must throw an exception if cwid is invalid
+											// add constructors, destructors, assignment operators if needed
+	
 private:
 	// Add private member variables for your class along with any 
 	// other variables required to implement the public member functions
 	// TO BE COMPLETED
-		struct node {
-		string cName;
-		string id;
-	        char sGrade;
-	        node *next;
-	};
-	node *head;
+
+	int counter = 0;
+	string courseName;
+	string cwid;
+	char grade;
+	Student *students[50000];
+	
 };
