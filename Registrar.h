70b5a2b8963525e61526cc88781f05491907c59f
ameyagovalkar <36211515+ameyagovalkar@users.noreply.github.com>
2018-02-26 11:48:14 -0800

    Update Registrar.h

--- Registrar.h
+++ Registrar.h
@@ -12,17 +12,18 @@ public:
 	Student& getStudent(string cwid) const; // return the Student object corresponding to a given CWID
 											// getStudent must throw an exception if cwid is invalid
 											// add constructors, destructors, assignment operators if needed
-	int size = 1;
+
+	Registrar();
 	~Registrar();
-	void deleteRegistrar() {
-		delete[] students;
-	}
+	Registrar(const Registrar& reg);
 
 private:
 	// Add private member variables for your class along with any 
 	// other variables required to implement the public member functions
 	// TO BE COMPLETED
 
-	Student *students = new Student[50000];
+	Student *students;
+	int size;
+
 
 };
