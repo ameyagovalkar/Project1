6ac8aff21f58d8a966ad3fad03fc940cb3788b29
ameyagovalkar <36211515+ameyagovalkar@users.noreply.github.com>
2018-02-26 16:32:18 -0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -6,19 +6,20 @@ using namespace std;
 class Student {
 public:
 	Student(); // default constructor
-	Student(const string &cwid); // constructor with parameter
+	Student(const string &cwid);
+	// constructor with parameter
 	~Student(); //deconstructor to delete the dynamically allocated arrays
 	void addCourseGrade(const string &courseName, char grade); // add course name and grade to student's record
 	double getGPA(); // calculate and return GPA
 	void printTranscript(); // print transcript - see Student.cpp for the format
 	string getCWID(); // return the CWID of this student
-
+	Student& Student::operator=(const Student& stu);
 	friend class Registrar;
 private:
 	// any private member variables and methods go here
 	// TO BE COMPLETED
 
-	string studentCWID;
+	string StudentCWID;
 	string * courses;
 	char * grades;
 	double gpa;
